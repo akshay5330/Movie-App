67638b31af8534506113896322a462d0b18fb3eb
Akshay <akshayg.itpath@gmail.com>
2023-10-16 16:10:55 +0530

    Some css changes

--- src/components/ContentModal/ContentModal.js
+++ src/components/ContentModal/ContentModal.js
@@ -21,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
     height: "80%",
     backgroundColor: "#39445a",
     border: "1px solid #282c34",
-    borderRadius: 10,
+    // borderRadius: 10,
     color: "white",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(1, 1, 3),
